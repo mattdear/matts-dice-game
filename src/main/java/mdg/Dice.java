36bfe78bdfbccb656ffcb90f989d7f2b983da659
Matt Dear <mrmdear@gmail.com>
2020-06-22 14:10:51 +0100

    Initial project upload.

---
+++ src/main/java/mdg/Dice.java
@@ -0,0 +1,5 @@
+package mdg;
+
+public class Dice {
+
+}
---
+++ src/test/java/mdg/DiceTesting.java
@@ -0,0 +1,14 @@
+package mdg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiceTesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
