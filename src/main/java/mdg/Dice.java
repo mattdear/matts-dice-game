7b7ffc80a0cb416e66ef2ed70dd2f75c9cfa0543
Matt Dear <mrmdear@gmail.com>
2020-06-24 15:54:51 +0100

    Dice (#1)
    * Initial project upload.
    
    * I have now created the Dice and DiceTest class.
    
    * Added developer header.

---
+++ src/main/java/mdg/Dice.java
@@ -0,0 +1,24 @@
+package mdg;
+
+/**
+ *  Developer: Matthew Dear
+ *  GitHub: http://www.github.com/mattdear
+ */
+
+public class Dice {
+
+	private int value;
+
+	public Dice(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
---
+++ src/test/java/mdg/DiceTest.java
@@ -0,0 +1,44 @@
+package mdg;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *  Developer: Matthew Dear
+ *  GitHub: http://www.github.com/mattdear
+ */
+
+public class DiceTest {
+      
+    @Test
+    public void testConstruct() {
+        int expResult = 1;
+        System.out.println("Start diceTestConstruct");
+        Dice instance = null;
+        assertNull(instance);
+        instance = new Dice(expResult);
+        assertNotNull(instance);
+        assertEquals(expResult, instance.getValue());
+    }
+
+    @Test
+    public void testGetValue() {
+        int expResult = 2;
+        System.out.println("Start diceTestGetValue");      
+        Dice instance = new Dice(expResult);
+        assertNotNull(instance);
+        assertEquals(expResult, instance.getValue());
+    }
+
+    @Test
+    public void testSetValue() {
+        int expResult = 3;
+        System.out.println("Start diceTestSetValue");
+        Dice instance = new Dice(1);
+        assertNotNull(instance);
+        assertEquals(1, instance.getValue());
+        instance.setValue(expResult);
+        assertEquals(expResult, instance.getValue());
+    }
+    
+}
